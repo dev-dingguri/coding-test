b02e6664f1784d72618ffb7c8e2b63db3a60b81a
dev-dingguri <100068248+dev-dingguri@users.noreply.github.com>
2023-04-21 08:21:21 +0900

    [programmers] 예상 대진표 (cpp, js)
    https://programmers.co.kr/learn/courses/30/lessons/12985
    설명 추가

--- programmers/an-expected-list-of-matches.js
+++ programmers/an-expected-list-of-matches.js
@@ -1,22 +1,26 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12985
 
-// best
-function solution(n, a, b) {
-  let answer = 0;
-  while (a !== b) {
-    ++answer;
-    a = Math.ceil(a / 2);
-    b = Math.ceil(b / 2);
+{
+  // best
+  function solution(n, a, b) {
+    let answer = 0;
+    while (a !== b) {
+      ++answer;
+      a = Math.ceil(a / 2); // 반올림을 위한 추가적인 조건이 필요 없기 때문에 더 빠름
+      b = Math.ceil(b / 2);
+    }
+    return answer;
   }
-  return answer;
 }
 
-function solution(n, a, b) {
-  let answer = 0;
-  while (a !== b) {
-    ++answer;
-    a = parseInt((a + 1) / 2); // JS에서 (a + 1) / 2는 실수임
-    b = parseInt((b + 1) / 2);
+{
+  function solution(n, a, b) {
+    let answer = 0;
+    while (a !== b) {
+      ++answer;
+      a = parseInt((a + 1) / 2); // JS에서 (a + 1) / 2는 실수
+      b = parseInt((b + 1) / 2);
+    }
+    return answer;
   }
-  return answer;
 }
